7197e18467ce2ce8d4ae460a3aca5a6fd8b33f2c
r1nozaki <r1nozaki@gmail.com>
2026-01-14 18:41:03 +0200

    Fix for Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
-import Loader from './components/UI/Loader.tsx';
-import { router } from './router/router.tsx';
+import Loader from './components/UI/Loader';
+import { router } from './router/router';
 
 import './App.css';
 
